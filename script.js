8f878a10d5b5b15b5642b58e50bd02951bb26d6c
Kaushik Thirthappa <kaushik@quadnode.com>
2012-03-27 15:42:45 +0530

    pushing it to git for anyone to use.

---
+++ script.js
@@ -0,0 +1,45 @@
+window.onload = initAll;
+var usedNums = new Array(76);
+
+function initAll() {
+  if (document.getElementById) {
+    document.getElementById("reload").onclick = anotherCard;
+    newCard();
+  }
+  else{
+    alert("Your browser does not support this script.");
+  }
+}
+
+function newCard() {
+  for(var i=0 ; i<24 ; i++){
+      setSquare(i);
+    }
+}
+
+function setSquare(thisSquare){
+  var currentSquare = "square" + thisSquare;
+  var colPlace = new Array(0,1,2,3,4,0,1,2,3,4,0,1,3,4,0,1,2,3,4,0,1,2,3,4);
+  var colBasis = colPlace[thisSquare] * 15;
+  var newNum = colBasis + getNewNum() + 1;
+
+  do{
+    newNum = colBasis + getNewNum() + 1;
+  }while(usedNums[newNum]);
+  
+  usedNums[newNum] = true;
+  document.getElementById(currentSquare).innerHTML = newNum;
+}
+
+function getNewNum() {
+  return Math.floor(Math.random() * 15);
+}
+
+function anotherCard() {
+  for (var i = 1; i < usedNums.length; i++) {
+    usedNums[i] = false;
+  };
+
+  newCard();
+  return false;
+}
